eb2ddb52465c9a7c0ade3a7702bdf835f7765543
gcmartin <gscalica@gmail.com>
2020-09-23 14:30:41 -0700

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,15 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div>
-      <ul>
-        <li><NavLink activeClassName="active-link" to="/">Home</NavLink></li>
-        <li><NavLink activeClassName="active-link" to="/chips">Chips</NavLink></li>
-        <li><NavLink activeClassName="active-link" to="/cookies">Cookies</NavLink></li>
-        <li><NavLink activeClassName="active-link" to="/candy">Candy</NavLink></li>
+    <div className="Navbar">
+      <ul className="Navbar-ul">
+        <div className="Navbar-lis">
+          <li><NavLink activeClassName="active-link" to="/">Home</NavLink></li>
+          <li><NavLink activeClassName="active-link" to="/chips">Chips</NavLink></li>
+          <li><NavLink activeClassName="active-link" to="/cookies">Cookies</NavLink></li>
+          <li><NavLink activeClassName="active-link" to="/candy">Candy</NavLink></li>
+        </div>
+        
       </ul>
     </div>
   );
