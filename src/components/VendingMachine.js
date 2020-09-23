777bf490c6c68011ca4b268f25133e1ab6efb311
gcmartin <gscalica@gmail.com>
2020-09-23 14:19:42 -0700

    Add vending machine image, style all images, and put links in a ul

--- src/components/VendingMachine.js
+++ src/components/VendingMachine.js
@@ -1,18 +1,27 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
 
 function VendingMachine() {
   return (
     <div>
       <h1>Hi!  I'm a vending machine.  What would you like to eat?</h1>
-      
-        <Link to="/chips">Chips</Link>
-        <Link to="/cookies">Cookies</Link>
-        <Link to="/candy">Candy</Link>
-  
+      <img src="https://images.unsplash.com/photo-1585341840941-98553e474d84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1276&q=80" alt="vending machine" />
+      <div>
+        <div>
+          <ul>
+            <li><Link to="/chips">Chips</Link></li>
+            <li><Link to="/cookies">Cookies</Link></li>
+            <li><Link to="/candy">Candy</Link></li>
+          </ul>
+        </div>
+      </div>
+
+
+
+
+
     </div>
-    
+
   );
 }
 
